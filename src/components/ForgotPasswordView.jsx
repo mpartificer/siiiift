b29bc31e8f974a2431c84aa91c024beae11292de
mpartificer <megan.forsey@hotmail.com>
2024-10-01 20:22:22 -0230

    Implemented Supabase emails for forgotten passwords

--- src/components/ForgotPasswordView.jsx
+++ src/components/ForgotPasswordView.jsx
@@ -1,17 +1,33 @@
 import '../App.css'
 import LogInGreeting from './multipurpose/LogInGreeting.jsx'
 import LogInSubGreeting from './multipurpose/LogInSubGreeting.jsx'
-import BigSubmitButton from './multipurpose/BigSubmitButton.jsx'
-import LoginEntry from './multipurpose/LoginEntry.jsx'
+import { useState, useEffect } from 'react'
+import { supabase } from '../supabaseClient.js'
+import {useNavigate} from 'react-router-dom'
 
 function ForgotPasswordView() {
+
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const navigate = useNavigate()
+
+  const handlePasswordReset = async (e) => {
+    console.log(email)
+    e.preventDefault()
+    setLoading(true)
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email)
+    
+    if (error) alert(error.message)
+    setLoading(false)
+  }
   return (
-    <div className='logInView'>
+    <form className='logInView' onSubmit={handlePasswordReset}>
       <LogInGreeting openingTitle='siiiift' />
       <LogInSubGreeting />
-      <LoginEntry entryValue='username' />
-      <BigSubmitButton submitValue='enter' path='/login'/>
-    </div>
+      <input className='loginBar' type='text' placeholder='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <button type="submit" className='bigSubmitButton' disabled={loading}>{loading ? 'loading...' : 'login'}</button>
+      </form>
   )
 }
 
