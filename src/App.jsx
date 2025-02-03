3cca3aa4acaa6a6d74babafbe4d2bd640de2d915
mpartificer <megan.forsey@hotmail.com>
2025-02-02 23:18:30 -0330

    did not import useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 import './App.css';
 import '../style.css';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RouterProvider } from "react-router-dom"
 import router from './Routes.jsx'
 import { AuthProvider } from './AuthContext.jsx'
